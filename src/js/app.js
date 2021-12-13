8843a855235a119b412902fb2cf3569c9a2a1e02
i.brusenko <i.brusenko@redefine.family>
2021-12-13 18:17:10 +0700

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+//= include '_base.js'
+
+if (document.readyState === 'loading') {
+  configOfEventListeners(false, {
+    target: window,
+    type: 'load',
+    func: initJs,
+  });
+} else {
+  initJs();
+}
+
+function initJs() {
+  // Тут начинается твой js-код
+}
