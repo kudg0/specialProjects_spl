5d61fdbdbdaa586f80d1059e9488dd3f7023ec08
i.brusenko <i.brusenko@redefine.family>
2021-12-20 19:42:09 +0700

    change load js method

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
-//= include '_base.js'
+(() => {
+  //= include '_base.js'
 
-if (document.readyState === 'loading') {
-  configOfEventListeners(false, {
-    target: window,
-    type: 'load',
-    func: initJs,
-  });
-} else {
-  initJs();
-}
+  if (document.readyState === 'loading') {
+    configOfEventListeners(false, {
+      target: window,
+      type: 'load',
+      func: initJs,
+    });
+  } else {
+    initJs();
+  }
 
-function initJs() {
-  // Тут начинается твой js-код
-}
+  function initJs() {
+    // Тут начинается твой js-код
+  }
+})();
